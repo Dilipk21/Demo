a84bf3c165ef5e35392c52bad7c37ed563b1ac7b
Dilipk21 <dilipkumarparthiban1995@gmail.com>
2022-06-08 08:21:29 +0530

    Kumaran updated his code

--- src/org/sample/GoodMorning.java
+++ src/org/sample/GoodMorning.java
@@ -6,6 +6,16 @@ public class GoodMorning {
 
 	}
 
+	public static void print2() {
+		System.out.println("Print 2");
+
+	}
+
+	public static void print3() {
+		System.out.println("Print3");
+
+	}
+
 	public static void main(String[] args) {
 
 	}
